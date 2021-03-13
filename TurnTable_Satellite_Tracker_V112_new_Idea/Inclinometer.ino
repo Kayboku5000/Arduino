8ced460474c36af68b0329dcd804ca0fdb3243cb
Kayboku5000 <62546159+Kayboku5000@users.noreply.github.com>
2021-03-13 19:53:12 +1100

    Create Inclinometer.ino

---
+++ TurnTable_Satellite_Tracker_V112_new_Idea/Inclinometer.ino
@@ -0,0 +1,15 @@
+
+
+
+
+void Inclinometer() {   // Elevation
+  sensors_event_t event;
+  bno.getEvent(&event);
+  Bn55 = (90 + ((float)event.orientation.y));
+  //Serial.print("Inclinometer (degrees): "); Serial.print(Bn55);
+  uint8_t sys, gyro, accel, mag = 0;
+  bno.getCalibration(&sys, &gyro, &accel, &mag);
+   delay(BNO055_SAMPLERATE_DELAY_MS);
+  
+
+}
