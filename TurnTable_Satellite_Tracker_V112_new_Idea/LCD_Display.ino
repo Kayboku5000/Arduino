6a013e7214aac90233a0ed146f48b7767eda7f51
Kayboku5000 <62546159+Kayboku5000@users.noreply.github.com>
2021-03-13 19:53:07 +1100

    Create LCD_Display.ino

---
+++ TurnTable_Satellite_Tracker_V112_new_Idea/LCD_Display.ino
@@ -0,0 +1,33 @@
+
+
+
+
+void lcdDisplay() {
+  if (headingDegrees > 0 && headingDegrees < 360) {
+    delay(10);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("");
+    lcd.setCursor(0, 0);
+    lcd.print(orbAzi);
+    lcd.setCursor(0, 1);  // set the cursor to column 0, line 1
+    lcd.print("");
+    lcd.setCursor(0, 1);             // (note: line 1 is the second row, since counting begins with 0):
+    lcd.print(orbElv);
+    //-------sensors-----------------
+    lcd.setCursor(7, 0);
+    lcd.print("A");
+    lcd.setCursor(10, 0);
+    lcd.print(headingDegrees);
+    lcd.setCursor(7, 1);  // set the cursor to column 0, line 1
+    lcd.print("E");
+    lcd.setCursor(10, 1);             // (note: line 1 is the second row, since counting begins with 0):
+    lcd.print(Bn55);
+
+
+  }
+  else {
+    lcd.setCursor(0, 1);
+    lcd.print("nuttin");
+  }
+}
